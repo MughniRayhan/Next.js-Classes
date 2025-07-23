b3cc11ce297072f464eea6b1bc4d84171a4eedb9
MughniRayhan <tishtisha13@gmail.com>
2025-07-24 00:42:28 +0600

    Get Currently Logged In user in Client and Server component

--- nextjs-auth/src/app/api/auth/[...nextauth]/route.js
+++ nextjs-auth/src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-const handler = NextAuth({
+
+export const authOptions = {
   providers: [
   CredentialsProvider({
     name: "Credentials",
-    
+
     credentials: {
       username: { label: "Username", type: "text", placeholder: "jsmith" },
       password: { label: "Password", type: "password" }
@@ -26,6 +27,8 @@ const handler = NextAuth({
     }
   })
 ]
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- nextjs-auth/src/app/page.js
+++ nextjs-auth/src/app/page.js
@@ -1,7 +1,11 @@
 import LoginButton from "@/components/LoginButton";
+import UserInfo from "@/components/UserInfo";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -51,7 +55,11 @@ export default function Home() {
             Read our docs
           </a>
           <LoginButton/>
+         
         </div>
+         <p className="font-semibold text-gray-200"> From client </p>
+          <UserInfo/>
+          {JSON.stringify(session) }
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
