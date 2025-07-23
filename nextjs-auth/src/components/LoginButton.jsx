033e95d502372281b694b1bd99b7f785106c92fd
MughniRayhan <tishtisha13@gmail.com>
2025-07-23 22:55:55 +0600

    create route for authentication and check signin user

--- nextjs-auth/src/components/LoginButton.jsx
+++ nextjs-auth/src/components/LoginButton.jsx
@@ -1,9 +1,13 @@
+"use client";
 import React from 'react'
+import {  signIn } from "next-auth/react"
 
 export default function LoginButton() {
   return (
     <div>
-      
+      <button onClick={() => signIn()}
+        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+        >Login</button>
     </div>
   )
 }
